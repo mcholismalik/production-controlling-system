55a1084304261804694573062a8e961eb29a0744
Cols <mcholismalik@gmail.com>
2019-03-01 22:50:19 +0700

    Fixing nav upper position

--- application/views/layout.php
+++ application/views/layout.php
@@ -1,12 +1,29 @@
-
 <?php 
 	$this->load->view('head');
 	$this->load->view('nav');
 ?>
-  	<body id="top">
-		<div id="wrapper">
-			<div id="page-wrapper" class="gray-bg">
-				<?php 
+
+<body id="top">
+	<div id="wrapper">
+		<div id="page-wrapper" class="gray-bg">
+			<div class="row border-bottom">
+				<nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
+					<div class="navbar-header">
+						<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+					</div>
+					<ul class="nav navbar-top-links navbar-right">
+						<li>
+							<span class="m-r-sm text-muted welcome-message">Production Controlling System</span>
+						</li>
+						<li>
+							<a href="login.html">
+								<i class="fa fa-sign-out"></i> Log out
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<?php 
 					// DUMMY
 					if ($page == 'beranda') {
 						$this->load->view('page/beranda');
@@ -42,9 +59,10 @@
 					}
 
 				?>
-				<?php $this->load->view('copyright'); ?>
-				<?php $this->load->view('footer'); ?>
-			</div>
+			<?php $this->load->view('copyright'); ?>
+			<?php $this->load->view('footer'); ?>
 		</div>
-  	</body>
+	</div>
+</body>
+
 </html>
