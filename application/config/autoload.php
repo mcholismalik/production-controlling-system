9638283b22a6ea6080bcd22b747a234cdaf50868
Cols <mcholismalik@gmail.com>
2019-03-03 00:32:42 +0700

    Delete library encrypt in autoload, it isn't work in php7

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'encrypt', 'upload', 'email', 'encryption');
+$autoload['libraries'] = array('database', 'session', 'upload', 'email', 'encryption');
 
 /*
 | -------------------------------------------------------------------
