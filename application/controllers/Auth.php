552155d3006fc7505e0660aba8dbf040df39047d
Cols <mcholismalik@gmail.com>
2019-03-02 23:48:59 +0700

    New encryption library for php7 support

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,8 +5,8 @@ class Auth extends CI_Controller {
 	public function __construct() 
 	{
         parent::__construct();
-        if ($this->session->userdata('id_user')) {
-        	redirect('beranda');
+        if (!$this->session->userdata('id_user')) {
+        	redirect('auth/login');
         }
 	}
 
@@ -25,7 +25,7 @@ class Auth extends CI_Controller {
 
 		if (!empty($cekUser)) {
 			foreach ($cekUser as $cu) {
-				$passwordDecrypt = $this->encrypt->decode($cu->password);
+				$passwordDecrypt = $this->encryption->decrypt($cu->password);
 			}
 			if ($password == $passwordDecrypt) {			
 				$cek = $this->M_login->getUser($username)->result();
@@ -54,5 +54,22 @@ class Auth extends CI_Controller {
 		$this->session->sess_destroy();
 		$this->session->set_flashdata('psn','Anda Telah Keluar');
 		redirect('auth/login');
-	}
+    }
+    
+
+    // this is new encryption run for php7, encrypt isn't support for php7
+    public function encrypt_php7() 
+    {   
+        $text = '123456';
+        $ciphertext = $this->encryption->encrypt($text);
+        echo $ciphertext;
+    }
+
+    public function decrypt_php7() 
+    {
+        $chipertext = '6656891601ff6c57da12e8cdabe3ba65921c1e08c50504fd8ece45fe965eeea198e1c83ff04cbfeedf58e19ca4b18bf67d9f47d1d7debcba27631b2bb02254d09DE1O9hTgP+DqBx2Vq4dA+zP9+c+NLnqzCSS7gImVNE=';
+        $text = $this->encryption->decrypt($chipertext);
+        echo $text;
+    }
+
 }
